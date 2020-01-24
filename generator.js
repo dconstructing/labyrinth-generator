43821f7d4b9e9e2e7b8ddce3d109c5b906cbb33f
David Cox <losttime.shuffle@gmail.com>
2020-01-23 22:50:59 -0700

    Specify number of walls desired

--- generator.js
+++ generator.js
@@ -1,6 +1,35 @@
-function maze(x,y) {
+const determineNeighbors = function(rows, columns, hereRow, hereColumn) {
+	var all = [];
+
+	for (var j = 0; j < rows; j++) {
+		all[j] = [];
+		for (var k = 0; k < columns; k++) {
+			all[j].push([j, k]);
+		}
+	}
+
+	// collect list of theoretical neighbors to current space
+	var potential = [
+		[hereRow+1, hereColumn], // right
+		[hereRow, hereColumn+1], // above
+		[hereRow-1, hereColumn], // left
+		[hereRow, hereColumn-1]  // below
+	];
+	var neighbors = [];
+	// whittle down list of theoretical neighbors to actual neighbors
+	for (var j = 0; j < 4; j++) {
+		if (all[potential[j][0]] && all[potential[j][0]][potential[j][1]]) {
+			neighbors.push(potential[j]);
+		}
+	}
+
+	return neighbors;
+};
+
+function maze(x , y, numberOfDesiredWalls) {
 	// total number of spaces on the grid
 	var spaces = x * y - 1;
+	var wallCount = ((x - 1) * y) + (x * (y - 1));
 
 	if (spaces < 0) {
 		alert("illegal maze dimensions");
@@ -12,14 +41,19 @@ function maze(x,y) {
 		here = [Math.floor(Math.random() * x), Math.floor(Math.random() * y)], // random starting location
 	    path = [here],
 	    unvisited = [];
-	
+
 	// Prepare the grid for traversal
 	for (var j = 0; j < x; j++) horiz[j] = [];
 	for (var j = 0; j < y-1; j++) verti[j] = [];
 	for (var j = 0; j < x + 2; j++) {
 		unvisited[j] = [];
 		for (var k = 0; k < y + 1; k++) {
-			unvisited[j].push(j > 0 && j < x + 1 && k > 0 && (j != here[0] + 1 || k != here[1] + 1));
+			// seems like it's intended to create a grid of 'true' surrounded by 'false's?
+			unvisited[j].push(j > 0 && // first row will all be false
+								j < x + 1 && // last row will all be false
+								k > 0 && // first cell of each row will be false
+								(j != here[0] + 1 ||
+									k != here[1] + 1));
 		}
 	}
 
@@ -51,10 +85,12 @@ function maze(x,y) {
 				// same column, different row ?
 				// horizontal movement is possible
 				horiz[next[0]][(next[1] + here[1] - 1) / 2] = true;
+				wallCount--;
 			} else {
 				// same row, different column ?
 				// vertical movement is possible
 				verti[(next[0] + here[0] - 1) / 2][next[1]] = true;
+				wallCount--;
 			}
 			path.push(here = next);
 		} else {
@@ -62,6 +98,40 @@ function maze(x,y) {
 			here = path.pop();
 		}
 	}
+
+	console.log('wall count', wallCount, numberOfDesiredWalls);
+	// remove random wall to get down to the number of desired walls
+	while (wallCount > numberOfDesiredWalls) {
+		console.log('removing random wall');
+		here = [Math.floor(Math.random() * x), Math.floor(Math.random() * y)]; // random space
+
+		const removalNeighbors = determineNeighbors(x, y, here[0], here[1]);
+		const removalNext = removalNeighbors[Math.floor(Math.random() * removalNeighbors.length)];
+
+		if (removalNext[0] == here[0]) {
+			// same row, different column
+			console.log('same row');
+			if (!horiz[removalNext[0]][(removalNext[1] + here[1] - 1) / 2]) {
+				horiz[removalNext[0]][(removalNext[1] + here[1] - 1) / 2] = true;
+				wallCount--;
+				console.log('wall removed');
+			} else {
+				console.log('nothing to remove');
+			}
+		} else {
+			// same column, different row
+			console.log('same column');
+			if (!verti[(removalNext[0] + here[0] - 1) / 2][removalNext[1]]) {
+				verti[(removalNext[0] + here[0] - 1) / 2][removalNext[1]] = true;
+				wallCount--;
+				console.log('wall removed');
+			} else {
+				console.log('nothing to remove');
+			}
+		}
+	}
+	console.log('final wall count', wallCount, numberOfDesiredWalls);
+
 	return {x: x, y: y, horiz: horiz, verti: verti};
 }
  
